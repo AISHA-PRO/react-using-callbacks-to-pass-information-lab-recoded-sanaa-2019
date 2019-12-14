8a6ef6d6bf6edf8d200aa76ad1bb4f37b85cad70
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-14 08:05:43 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -13,7 +13,8 @@ export default class Matrix extends Component {
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
+)
   )
 
   genMatrix = () => (
